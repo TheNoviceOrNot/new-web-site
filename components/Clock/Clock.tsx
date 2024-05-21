b7a1197730fd1d185a59ebc5d116f1f7dae97ce5
TheNoviceOrNot <wang_brian@icloud.com>
2024-05-21 17:49:51 -0400

    Counter

--- components/Clock/Clock.tsx
+++ components/Clock/Clock.tsx
@@ -1,6 +1,17 @@
-import styles from "./clock.module.css";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Clock() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    })
+    
     return (
         <div> 
             <h2> Current Date: {new Date().toLocaleDateString()} </h2>
